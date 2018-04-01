996a348a2265a7699d717521e4db3d249ae9ee03
yangzhenkun <yangzhenkun@bogon>
2018-04-01 20:44:01 +0800

    LRU算法/计算tree的最大深度算法实现

---
+++ src/main/java/com/yasin/algorithm/LRUTest.java
@@ -0,0 +1,26 @@
+package com.yasin.algorithm;
+
+import org.junit.Test;
+
+public class LRUTest {
+
+	
+	@Test
+	public void testLRU() {
+		
+		LRU lru = new LRU(5);
+		
+		for(int i=0;i<10;i++) {
+			lru.set(i+"", i);
+			lru.get("0");
+		}
+		
+		
+		for(int i=0;i<10;i++) {
+			System.out.println(lru.get(""+i));
+		}
+		
+	}
+	
+	
+}
