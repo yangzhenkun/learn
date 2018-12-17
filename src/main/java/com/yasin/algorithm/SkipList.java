cca2604e0e0312c58d5456405e9db7a3da709dd4
yangzhenkun <1334036616@qq.com>
2018-12-17 11:14:55 +0800

    skiplist实现

--- src/main/java/com/yasin/algorithm/SkipList.java
+++ src/main/java/com/yasin/algorithm/SkipList.java
@@ -1,40 +1,140 @@
 package com.yasin.algorithm;
 
-import lombok.Data;
-
 /**
  * @author yangzhenkun
  * @create 2018-12-07 18:10
  */
-@Data
 public class SkipList<T> {
 
-    private int cap = 65536;//2^16
+    private static final int DEFAULT_CAP = 65536;
+    private int cap = 0;//2^16
     private int maxLevel = 8;
-    private double p = 0.25;
+    private final double p = 0.25;
+    private SkipNode head;
+
 
     public SkipList(int cap) {
-
         this.cap = cap;
         this.maxLevel = maxLevel(cap);
+
+        head = new SkipNode();
+        head.key = null;
+        head.value = null;
+        head.forward = new SkipNode[maxLevel];
+
     }
 
     public SkipList() {
+        this(DEFAULT_CAP);
     }
 
-    public T get(int key){
+    public int getMaxLevel() {
+        return this.maxLevel;
+    }
 
+    public T get(Integer key) {
+
+        SkipNode cur = head;
+
+        for (int l = maxLevel - 1; l >= 0; l--) {
+
+            if (cur.forward[l] == null) {
+                continue;
+            }
+
+            while (cur.forward[l] != null && key > cur.forward[l].key) {
+                cur = cur.forward[l];
+            }
+
+            if (cur.forward[l] != null && key == cur.forward[l].key) {
+                return (T) cur.forward[l].value;
+            }
+        }
         return null;
     }
 
-    public boolean set(int key,T value){
+    public boolean set(int key, T value) {
+        SkipNode newNode = initNode(key, value);
+        int newNodeMaxLevel = newNode.forward.length;
+        SkipNode cur = head;
+        SkipNode[] update = new SkipNode[newNodeMaxLevel];//记录更新的节点
 
-        return false;
+        /**
+         * 遍历找到插入的节点，并记录下需要更新层指针的节点
+         */
+        for (int l = maxLevel - 1; l >= 0; l--) {
+
+            if (cur.forward[l] == null) {
+                if (l < newNodeMaxLevel) {
+                    update[l] = cur;
+                }
+                continue;
+            }
+
+            while (cur.forward[l] != null && key > cur.forward[l].key) {
+                cur = cur.forward[l];
+            }
+
+            if (cur.forward[l] != null && key == cur.forward[l].key) {
+                return false;
+            }
+            if (l < newNodeMaxLevel) {
+                update[l] = cur;
+            }
+        }
+        /**
+         * 执行插入更新操作
+         */
+        for (int l = newNodeMaxLevel - 1; l >= 0; l--) {
+            newNode.forward[l] = update[l].forward[l];
+            update[l].forward[l] = newNode;
+        }
+
+
+        return true;
     }
 
-    public boolean remove(int key){
+    public boolean remove(int key) {
+        SkipNode cur = head;
+        SkipNode[] update = new SkipNode[maxLevel];
+        SkipNode delete = null;
 
-        return false;
+        /**
+         * 遍历找到要删除的节点，并记录下需要更新层指针的节点
+         */
+        for (int l = maxLevel - 1; l >= 0; l--) {
+
+            if (cur.forward[l] == null) {
+                continue;
+            }
+
+            while (cur.forward[l] != null && key > cur.forward[l].key) {
+                cur = cur.forward[l];
+            }
+
+            if (cur.forward[l] != null && key == cur.forward[l].key) {
+                delete = cur.forward[l];
+                update[l] = cur;
+            }
+
+        }
+        /**
+         * 执行删除更新操作
+         */
+        if (delete == null) {
+            return false;
+        }
+
+        for (int l = delete.forward.length - 1; l >= 0; l--) {
+            update[l].forward[l] = delete.forward[l];
+            delete.forward[l] = null;//help gc
+        }
+
+        return true;
+    }
+
+    public boolean isEmpty() {
+        return head.forward[0] == null ? true : false;
     }
 
 
@@ -51,10 +151,63 @@ public class SkipList<T> {
         return (int) Math.ceil(level);
     }
 
+    /**
+     * 随机获得节点层数
+     *
+     * @return
+     */
+    private int randomLevel() {
+        int level = 1;
+        while (Math.random() <= p && level < maxLevel) {
+            level++;
+        }
+        return level;
+    }
+
+    /**
+     * 初始化节点
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    private SkipNode initNode(Integer key, T value) {
+
+        SkipNode node = new SkipNode();
+        node.key = key;
+        node.value = value;
+        int level = randomLevel();
+        node.forward = new SkipNode[level];
+        return node;
+    }
+
 
     private class SkipNode<T> {
 
-
+        SkipNode[] forward;
+        Integer key;
+        T value;
     }
+
+    @Override
+    public String toString() {
+        SkipNode cur = null;
+        StringBuilder sb = new StringBuilder();
+        for (int l = maxLevel - 1; l >= 0; l--) {
+            sb.append("第").append(l).append("层:");
+            cur = head.forward[l];
+
+            while (cur != null) {
+                sb.append(cur.key).append(";");
+
+                cur = cur.forward[l];
+            }
+
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
 
 }
--- src/test/java/com/yasin/jdklearn/JavaLearn/algorithm/SkipListTest.java
+++ src/test/java/com/yasin/jdklearn/JavaLearn/algorithm/SkipListTest.java
@@ -1,8 +1,8 @@
 package com.yasin.jdklearn.JavaLearn.algorithm;
 
+import com.yasin.algorithm.SkipList;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
-
-import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * @author yangzhenkun
@@ -13,12 +13,67 @@ public class SkipListTest {
 
     @Test
     public void sdd() {
+        SkipList<String> list = new SkipList<>(1024);
+        for (int i = 0; i < 50; i++) {
+            list.set((i * 2) + 1, String.valueOf((i * 2) + 1));
+        }
 
-//        SkipList<String> skipList = new SkipList(16);
+        for (int i = 0; i < 50; i++) {
+            list.set((i * 2), String.valueOf((i * 2)));
+        }
 
-        ConcurrentSkipListMap<Integer,String> set = new ConcurrentSkipListMap<>();
+        /**
+         * test add\get
+         */
+        for (int i = 0; i < 100; i++) {
+            System.out.println(list.get(i));
+        }
 
-        set.put(1,"1");
+        System.out.println(list.toString());
+        /**
+         *test remove
+         */
+        for (int i = 99; i >= 0; i--) {
+            System.out.println(list.remove(i));
+            for (int j = 0; j < i; j++) {
+                String v = list.get(j);
+                if (StringUtils.isBlank(v)) {
+                    System.out.println("error！删除" + i + "节点后，" + j + "的数据也没有了");
+                }
+                continue;
+            }
+        }
+
+        System.out.println("是否为空:" + list.isEmpty());
+
+        System.out.println(list.toString());
+
+
+        for (int i = 0; i < 50; i++) {
+            list.set((i * 2) + 1, String.valueOf((i * 2) + 1));
+        }
+
+        for (int i = 0; i < 50; i++) {
+            list.set((i * 2), String.valueOf((i * 2)));
+        }
+
+        /**
+         *test remove
+         */
+        for (int i = 0; i < 100; i++) {
+            System.out.println(list.remove(i));
+            for (int j = i + 1; j < 100; j++) {
+                String v = list.get(j);
+                if (StringUtils.isBlank(v)) {
+                    System.out.println("error！删除" + i + "节点后，" + j + "的数据也没有了");
+                }
+                continue;
+            }
+        }
+
+        System.out.println("是否为空:" + list.isEmpty());
+
+        System.out.println(list.toString());
 
     }
 
