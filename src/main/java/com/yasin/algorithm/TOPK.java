45b52fe98f5d820f4c10c29d7ae833ab3b92dd75
yangzhenkun <1334036616@qq.com>
2019-02-14 17:37:10 +0800

    topk

---
+++ src/main/java/com/yasin/algorithm/TOPK.java
@@ -0,0 +1,93 @@
+package com.yasin.algorithm;
+
+import org.junit.Test;
+
+import java.util.TreeSet;
+
+/**
+ * @author yangzhenkun
+ * @create 2019-02-14 10:42
+ */
+public class TOPK {
+
+
+
+    @Test
+    public void test() {
+
+        int[] array = {1, 10, 2, 20, 3, 30, 4, 40, 5, 50, 6, 60, 7, 70, 8, 80, 9, 90};
+        int k = 10;
+
+        topk2(array, k);
+
+        for (int i = 0; i < k; i++) {
+            System.out.println(array[i]);
+        }
+
+    }
+
+    //单次排序
+    private int partion(int[] data, int low, int high) {
+        int mid = data[low];
+        while (low < high) {
+            while (low < high && data[high] <= mid) {
+                high--;
+            }
+            if (low < high) {
+                data[low] = data[high];
+                low++;
+            }
+            while (low < high && data[low] >= mid) {
+                low++;
+            }
+            if (low < high) {
+                data[high] = data[low];
+                high--;
+            }
+        }
+
+        data[low] = mid;
+
+        return low;
+    }
+
+    private void topk2(int[] data, int k) {
+        k--;//因为数组从0开始存储，对k减一
+        int high = data.length - 1;
+        int low = 0;
+        int index = partion(data, low, high);
+
+        while (index != k) {
+
+            if (index > k) {
+                high = index - 1;
+            } else {
+                low = index + 1;
+            }
+            index = partion(data, low, high);
+
+        }
+
+    }
+
+
+    private TreeSet<Integer> topk(int[] array, int n) {
+        TreeSet<Integer> set = new TreeSet<Integer>();
+        for (int i = 0; i < array.length; i++) {
+            int value = array[i];
+            if (set.size() < n) set.add(value);
+            else {
+                int min = set.first();
+                if (value > min) {
+                    set.remove(min);
+                    set.add(value);
+                }
+            }
+        }
+        return set;
+    }
+
+
+
+
+}
